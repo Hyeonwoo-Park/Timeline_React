b776885c15f6ef44738af4ffaf875f69b92082e1
HWP <sunghakzz@naver.com>
2019-07-28 02:35:28 +0900

    무한스크롤 추가

--- src/Containers/PostContainer/PostContainer.js
+++ src/Containers/PostContainer/PostContainer.js
@@ -8,36 +8,52 @@ class PostContainer extends Component {
         // initializes component state
         this.state = {
             fetching: false, // tells whether the request is waiting for response or not
-            postList: [
-                {
-                    contentImg: null,
-                    contentText: null,
-                    createdDT: null,
-                    id: 0,
-                    updatedDT: null,
-                    writerId: null,
-                    writerImg: null,
-                    writerNickName: null
-                }
-            ]
+            page:0,
+            isLast: false,
+            postList: []
         };
     }
 
     fetchPostInfo = async (postId) => {
-        const post = await service.getBoard(0,5,3);
-        console.log(post);
-        const postList = post.data._embedded.boardReadDTOList;
         this.setState({
-            fetching: true,
-            postList
+            fetching: true
+        })
+        const post = await service.getBoard(postId,3);
+        console.log(post);
+        const postList = this.state.postList.concat(post.data._embedded.boardReadDTOList);
+        console.log(postList);
+        const isLast = (postId + 1 === post.data.page.totalPages);
+        this.setState({
+            fetching: false,
+            postList,
+            isLast,
+            page: postId+1
         });
     }
 
+    handleScroll = () =>{
+        const {innerHeight} = window;
+        const {scrollHeight} = document.body;
 
+        const scrollTop = (document.documentElement && document.documentElement.scrollTop) ||
+                            document.body.scrollTop;
+
+        if (scrollHeight - innerHeight - scrollTop <100 && this.state.fetching === false && this.state.isLast === false) {
+            this.fetchPostInfo(this.state.page);
+        }
+    }
 
     componentDidMount() {
-        this.fetchPostInfo(3);
+        this.fetchPostInfo(this.state.page);
+
+        window.addEventListener("scroll", this.handleScroll);
     }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    
     
     render() {
         return (
--- src/Services/Board.js
+++ src/Services/Board.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export function getBoard(page,size,writer) {
-    return axios.get(`http://localhost:8080/api/boards/?page=${page}&size=${size}&writer=${writer}`);
+export function getBoard(page,writer) {
+    return axios.get(`http://localhost:8080/api/boards/?page=${page}&size=5&writer=${writer}`);
 }
